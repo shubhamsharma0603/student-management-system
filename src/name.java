e57650e65c05c41b43c3c2ec473de351cc2b9c28
shubhamsharma0603 <139748127+shubhamsharma0603@users.noreply.github.com>
2026-05-13 20:23:19 +0530

    Update name.java

--- src/name.java
+++ src/name.java
@@ -2,6 +2,5 @@ public class name{
     public static void main(String[] args) {
         System.out.println("My name is Shubham");
         System.out.println("I am learning java");
-        System.out.println("I will get a job soon!");
     }
 }
